a506bc9d3e313e9baa967366eee0a7323efba544
Datior <leonardowederveiga@yahoo.com>
2023-04-21 00:10:50 -0300

    Tipos primitivos básicos

---
+++ type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "ola mundo"
+const texto2 = 'ola mundo'
+const senha = 'senhaSuperSegura'
+const StringDeNumeros = "34567"
+
+const citacao = 'O leo disse: '
+const leoDisse = 'Olá, Mundo!'
+console.log(citacao)
+
+// concatenacao (+)
+console.log(citacao + leoDisse)
+
+
+// templet string ou template literal
+
